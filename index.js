eb477b1af3e5ca9c83d772c0ecbb9cc0f82feadd
Sriram Mahadevan <srirammahadevan@outlook.com>
2020-08-01 20:57:09 +0530

    Added express to static serve

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app= express();
+
+app.use(express.static('./public'));
+
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+    console.log(`Listening at hhtp://localhost:${port}`)
+});
